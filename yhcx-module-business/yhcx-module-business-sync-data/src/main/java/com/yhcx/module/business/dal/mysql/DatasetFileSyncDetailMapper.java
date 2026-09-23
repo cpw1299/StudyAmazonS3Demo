3da6b90aef9e4d9964ae446528cc368dbcf089c1
Cuipw <52267035+cpw1299@users.noreply.github.com>
2026-09-23 11:26:22 +0800

    feat(sync-data): add dataset file sync persistence

---
+++ yhcx-module-business/yhcx-module-business-sync-data/src/main/java/com/yhcx/module/business/dal/mysql/DatasetFileSyncDetailMapper.java
@@ -0,0 +1,9 @@
+package com.yhcx.module.business.dal.mysql;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.yhcx.module.business.dal.dataobject.DatasetFileSyncDetailDO;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface DatasetFileSyncDetailMapper extends BaseMapper<DatasetFileSyncDetailDO> {
+}
